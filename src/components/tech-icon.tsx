78347f82d574ff05f574b4667174ece4f627457d
Francisco <franciscotrianto@gmail.com>
2024-11-21 08:56:44 +0700

    feat: content update 21-11-2024

--- src/components/tech-icon.tsx
+++ src/components/tech-icon.tsx
@@ -1,106 +1,65 @@
 import { FC } from 'react'
-import {
-  SiJavascript,
-  SiTypescript,
-  SiRuby,
-  SiCsharp,
-  SiPhp,
-  SiRust,
-  SiHtml5,
-  SiCss3,
-  SiReact,
-  SiNextdotjs,
-  SiNodedotjs,
-  SiFirebase,
-  SiMongodb,
-  SiMysql,
-  SiPostgresql,
-  SiSqlite,
-  SiTailwindcss,
-  SiC,
-  SiCplusplus,
-  SiMake,
-  SiNumpy,
-  SiResend,
-  SiShadcnui,
-  SiMdx,
-  SiVercel,
-  SiSass,
-  SiXampp,
-  SiFigma,
-  SiOpencv,
-  SiExpress,
-  SiMariadb,
-  SiQemu,
-  SiFlask
-} from 'react-icons/si'
-import {
-  FaJava,
-  FaPython,
-  FaDocker,
-  FaNode,
-  FaGoogle,
-  FaBootstrap
-} from 'react-icons/fa'
+import { IconType } from 'react-icons'
+import * as Si from 'react-icons/si'
+import * as Fa from 'react-icons/fa'
 import { FaGolang } from 'react-icons/fa6'
-import { twMerge } from 'tailwind-merge'
 import { FiFramer } from 'react-icons/fi'
 import { HiMiniCommandLine } from 'react-icons/hi2'
-import { DiProlog } from 'react-icons/di'
+import { DiProlog, DiGnu } from 'react-icons/di'
 import { MdOutlineFormatShapes, MdWeb } from 'react-icons/md'
 import { GrSystem } from 'react-icons/gr'
-import { DiGnu } from 'react-icons/di'
 import { TbHexagonLetterRFilled } from 'react-icons/tb'
+import { twMerge } from 'tailwind-merge'
 
-const techIconMap: { [key: string]: FC<any> } = {
-  javascript: SiJavascript,
-  typescript: SiTypescript,
-  python: FaPython,
-  ruby: SiRuby,
-  'c#': SiCsharp,
-  php: SiPhp,
+const techIconMap: Record<string, IconType> = {
+  javascript: Si.SiJavascript,
+  typescript: Si.SiTypescript,
+  python: Fa.FaPython,
+  ruby: Si.SiRuby,
+  'c#': Si.SiCsharp,
+  php: Si.SiPhp,
   go: FaGolang,
-  rust: SiRust,
-  html: SiHtml5,
-  css: SiCss3,
-  react: SiReact,
-  nextjs: SiNextdotjs,
-  nodejs: SiNodedotjs,
-  firebase: SiFirebase,
-  mongodb: SiMongodb,
-  mysql: SiMysql,
-  postgresql: SiPostgresql,
-  sqlite: SiSqlite,
-  tailwind: SiTailwindcss,
-  java: FaJava,
-  c: SiC,
-  cpp: SiCplusplus,
-  make: SiMake,
-  numpy: SiNumpy,
-  docker: FaDocker,
-  resend: SiResend,
-  shadcn: SiShadcnui,
-  mdx: SiMdx,
-  node: FaNode,
-  vercel: SiVercel,
-  sass: SiSass,
-  xampp: SiXampp,
-  figma: SiFigma,
-  opencv: SiOpencv,
-  tesserac: FaGoogle,
-  imutils: FaPython,
-  bootstrap: FaBootstrap,
+  rust: Si.SiRust,
+  html: Si.SiHtml5,
+  css: Si.SiCss3,
+  react: Si.SiReact,
+  nextjs: Si.SiNextdotjs,
+  nodejs: Si.SiNodedotjs,
+  firebase: Si.SiFirebase,
+  mongodb: Si.SiMongodb,
+  mysql: Si.SiMysql,
+  postgresql: Si.SiPostgresql,
+  sqlite: Si.SiSqlite,
+  tailwind: Si.SiTailwindcss,
+  java: Fa.FaJava,
+  c: Si.SiC,
+  cpp: Si.SiCplusplus,
+  make: Si.SiMake,
+  numpy: Si.SiNumpy,
+  docker: Fa.FaDocker,
+  resend: Si.SiResend,
+  shadcn: Si.SiShadcnui,
+  mdx: Si.SiMdx,
+  node: Fa.FaNode,
+  vercel: Si.SiVercel,
+  sass: Si.SiSass,
+  xampp: Si.SiXampp,
+  figma: Si.SiFigma,
+  opencv: Si.SiOpencv,
+  tesserac: Fa.FaGoogle,
+  imutils: Fa.FaPython,
+  bootstrap: Fa.FaBootstrap,
   'framer motion': FiFramer,
-  express: SiExpress,
+  express: Si.SiExpress,
   cli: HiMiniCommandLine,
   prolog: DiProlog,
   gui: MdOutlineFormatShapes,
   web: MdWeb,
-  mariadb: SiMariadb,
+  mariadb: Si.SiMariadb,
   os: GrSystem,
   gnu: DiGnu,
-  qemu: SiQemu,
-  flask: SiFlask,
+  qemu: Si.SiQemu,
+  flask: Si.SiFlask,
   rest: TbHexagonLetterRFilled
 }
 
@@ -116,32 +75,19 @@ const TechIcon: FC<TechIconProps> = ({
   containerClassName
 }) => {
   const Icon = techIconMap[tech.toLowerCase()]
+  const displayName =
+    tech === 'go'
+      ? 'Golang'
+      : tech === 'cli'
+        ? 'CLI'
+        : tech === 'c'
+          ? 'C Language'
+          : tech.charAt(0).toUpperCase() + tech.slice(1)
 
   return (
-    <div>
-      <div
-        className={twMerge(
-          'flex items-center justify-center gap-1',
-          containerClassName
-        )}
-      >
-        {Icon ? (
-          <>
-            <Icon className={iconClassName} />
-            <span>
-              {tech === 'go'
-                ? 'Golang'
-                : tech === 'cli'
-                  ? 'CLI'
-                  : tech === 'c'
-                    ? 'Language'
-                    : tech.charAt(0).toUpperCase() + tech.slice(1)}
-            </span>
-          </>
-        ) : (
-          <span>{tech.charAt(0).toUpperCase() + tech.slice(1)}</span>
-        )}
-      </div>
+    <div className={twMerge('flex items-center gap-1', containerClassName)}>
+      {Icon && <Icon className={iconClassName} />}
+      <span className='align-middle'>{displayName}</span>
     </div>
   )
 }
